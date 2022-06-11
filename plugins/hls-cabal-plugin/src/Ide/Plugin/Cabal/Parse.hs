d009aaaafff535b32ec8efd692f7f380707dd4dc
Rune K. Svendsen <runesvend@gmail.com>
2022-06-11 16:25:02 +0200

    hls-cabal-plugin: add module Ide.Plugin.Cabal.Parse

---
+++ plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/Parse.hs
@@ -0,0 +1,32 @@
+module Ide.Plugin.Cabal.Parse
+( parseCabalFile
+) where
+
+import Distribution.PackageDescription.Parsec
+    ( parseGenericPackageDescription )
+import Distribution.Types.GenericPackageDescription
+    ( GenericPackageDescription )
+import qualified Data.ByteString as BS
+import Distribution.Fields.ParseResult (runParseResult)
+import Distribution.Fields (PWarning, PError)
+import qualified System.Directory as Dir
+import Control.Monad (unless)
+import qualified System.Exit as Exit
+import Distribution.Types.Version (Version)
+import Data.List.NonEmpty (NonEmpty)
+
+
+parseCabalFile
+    :: FilePath
+    -> IO ([PWarning], Either (Maybe Version, NonEmpty PError) GenericPackageDescription)
+parseCabalFile =
+    readGenericPackageDescription'
+  where
+    readGenericPackageDescription' = readAndParseFile' parseGenericPackageDescription
+    readAndParseFile' parser fpath = do
+        exists <- Dir.doesFileExist fpath
+        unless exists $
+            Exit.die $
+                "Error Parsing: file \"" ++ fpath ++ "\" doesn't exist. Cannot continue."
+        bs <- BS.readFile fpath
+        pure $ runParseResult (parser bs)
