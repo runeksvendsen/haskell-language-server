b979730a945e3b77f5bba75974d79c8a59d8b36a
Rune K. Svendsen <runesvend@gmail.com>
2022-06-13 12:05:41 +0200

    Remove codeLens

--- plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal.hs
+++ plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts      #-}
@@ -7,7 +6,6 @@
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE ViewPatterns          #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE PolyKinds             #-}
@@ -16,21 +14,17 @@
 module Ide.Plugin.Cabal where
 
 import           Control.Monad.IO.Class
-import           Data.Aeson
 import qualified Data.Text as T
 import           Development.IDE            as D
 import           GHC.Generics
-import           Ide.PluginUtils
 import           Ide.Types
 import           Language.LSP.Types
 import qualified Ide.Plugin.Cabal.Parse as Parse
 import qualified Ide.Plugin.Cabal.Diag as Diag
 import qualified Data.List.NonEmpty as NE
-import qualified Data.HashMap.Strict as Map
 import Control.DeepSeq (NFData)
 import           Data.Hashable
 import           Data.Typeable
-import           Language.LSP.Server
 import qualified Development.IDE.Core.Shake as Shake
 
 
@@ -56,8 +50,6 @@ instance Pretty Log where
 descriptor :: Recorder (WithPriority Log) -> PluginId -> PluginDescriptor IdeState
 descriptor recorder plId = (defaultCabalPluginDescriptor plId)
   { pluginRules = exampleRules recorder
-  , pluginHandlers = mkPluginHandler STextDocumentCodeLens (codeLens recorder)
-  , pluginCommands = [PluginCommand commandId' "example adding" addTodoCmd]
   , pluginNotificationHandlers = mconcat
   [ mkPluginNotificationHandler LSP.STextDocumentDidOpen $
       \ide vfs _ (DidOpenTextDocumentParams TextDocumentItem{_uri,_version}) -> liftIO $ do
@@ -135,44 +127,3 @@ exampleRules recorder = do
 
 commandId' :: CommandId
 commandId' = "codelens.cabal.parse"
-
-codeLens :: Recorder (WithPriority Log) -> PluginMethodHandler IdeState 'TextDocumentCodeLens
-codeLens recorder _ideState plId CodeLensParams{_textDocument=TextDocumentIdentifier uri} = liftIO $ do
-    log' Debug $ LogText "ExampleCabal.codeLens entered (ideLogger)"
-    case uriToFilePath' uri of
-      Just (toNormalizedFilePath -> filePath) -> do
-        res <- Parse.parseCabalFile (fromNormalizedFilePath filePath)
-        let a = case res of
-              (_, Left (_, pErrorNE)) ->
-                let diagLst = NE.map (Diag.errorDiag filePath) pErrorNE
-                in List $ NE.toList $ NE.map (\(_, _, diag') -> mkCodeLens diag') diagLst
-              _ -> List []
-        pure $ Right a
-      Nothing -> pure $ Right $ List []
-  where
-    log' = logWith recorder
-    mkCodeLens diag' =
-      let
-        cmdParams = AddTodoParams uri diag'
-        cmd = mkLspCommand plId commandId' title (Just [toJSON cmdParams])
-        title = "ERROR: " <> Diag._message diag'
-      in CodeLens (Diag._range diag') (Just cmd) Nothing
-data AddTodoParams = AddTodoParams
-  { file     :: Uri  -- ^ Uri of the file to add the pragma to
-  , diag :: Diagnostic
-  }
-  deriving (Show, Eq, Generic, ToJSON, FromJSON)
-
-addTodoCmd :: CommandFunction IdeState AddTodoParams
-addTodoCmd _ide (AddTodoParams uri diag') = do
-  let
-    textEdits = List
-      [TextEdit (Diag._range diag')
-                  ("-- Parser error:" <> Diag._message diag' <> "\n")
-      ]
-    res = WorkspaceEdit
-      (Just $ Map.singleton uri textEdits)
-      Nothing
-      Nothing
-  _ <- sendRequest SWorkspaceApplyEdit (ApplyWorkspaceEditParams Nothing res) (\_ -> pure ())
-  return $ Right Null
